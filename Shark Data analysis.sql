5658fec3d9db87d17528087107a09eb923b5f594
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-30 10:29:29 +0200

    Update Shark Data analysis.sql

--- Shark Data analysis.sql
+++ Shark Data analysis.sql
@@ -1,13 +1,22 @@
+SELECT * FROM `Sharks Tank`;
+
 -- 1. What is the distribution of startups across different seasons of Shark Tank?
-
+SELECT `Season Number`, count(`Startup Name`) `count of pitchers`
+FROM `Sharks Tank`
+GROUP BY `Season Number`
+ORDER BY `count of pitchers` DESC;
 -- 2. What industries were most commonly represented in the show?
-
+SELECT Industry, count(industry) `count of industry`
+FROM `Sharks Tank`
+GROUP BY Industry
+ORDER BY `count of industry` DESC;
 -- 3. How was the gender distribution among the entrepreneurs who pitched their startups?
+
 -- 4. Which city and state were most entrepreneurs from?
 
 -- 5. How many of the pitches actually got a deal with the sharks?
 -- 6. What was the total amount secured by startups in deals?
--- 7. What was the total equity given up by startups in deals?
+-- 7. What was the total Average equity given up by startups in each indutries when got deals?
 
 
 --  8. How much did Barbara Corcoran typically invest in startups, and what equity did she receive?
