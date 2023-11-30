5f9fe5fe340fca29f1dea35e3f3493ce4e4cb058
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-30 09:43:40 +0200

    Update Shark Data analysis.sql

--- Shark Data analysis.sql
+++ Shark Data analysis.sql
@@ -1,18 +1,18 @@
 -- 1. What is the distribution of startups across different seasons of Shark Tank?
 
--- 7. What industries were most commonly represented in the show?
+-- 2. What industries were most commonly represented in the show?
 
--- 9. How was the gender distribution among the entrepreneurs who pitched their startups?
--- 10. Which city and state were most entrepreneurs from?
+-- 3. How was the gender distribution among the entrepreneurs who pitched their startups?
+-- 4. Which city and state were most entrepreneurs from?
 
- How many of the pitches actually got a deal with the sharks?
-18. What was the total amount secured by startups in deals?
-19. What was the total equity given up by startups in deals?
+-- 5. How many of the pitches actually got a deal with the sharks?
+-- 6. What was the total amount secured by startups in deals?
+-- 7. What was the total equity given up by startups in deals?
 
 
- How much did Barbara Corcoran typically invest in startups, and what equity did she receive?
-27. How much did Mark Cuban typically invest in startups, and what equity did he receive?
-28. How much did Lori Greiner typically invest in startups, and what equity did she receive?
-29. How much did Robert Herjavec typically invest in startups, and what equity did he receive?
-30. How much did Daymond John typically invest in startups, and what equity did he receive?
-31. How much did Kevin O'Leary typically invest in startups, and what equity did he receive?
+--  8. How much did Barbara Corcoran typically invest in startups, and what equity did she receive?
+-- 9. How much did Mark Cuban typically invest in startups, and what equity did he receive?
+-- 10. How much did Lori Greiner typically invest in startups, and what equity did she receive?
+-- 11. How much did Robert Herjavec typically invest in startups, and what equity did he receive?
+-- 12. How much did Daymond John typically invest in startups, and what equity did he receive?
+-- 13. How much did Kevin O'Leary typically invest in startups, and what equity did he receive?
