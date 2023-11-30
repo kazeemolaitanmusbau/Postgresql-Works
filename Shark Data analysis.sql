9f78ccc6f01ec9320b63435a13ab0ffb1ccf5b06
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-30 09:41:43 +0200

    Update Shark Data analysis.sql

--- Shark Data analysis.sql
+++ Shark Data analysis.sql
@@ -8,3 +8,11 @@
  How many of the pitches actually got a deal with the sharks?
 18. What was the total amount secured by startups in deals?
 19. What was the total equity given up by startups in deals?
+
+
+ How much did Barbara Corcoran typically invest in startups, and what equity did she receive?
+27. How much did Mark Cuban typically invest in startups, and what equity did he receive?
+28. How much did Lori Greiner typically invest in startups, and what equity did she receive?
+29. How much did Robert Herjavec typically invest in startups, and what equity did he receive?
+30. How much did Daymond John typically invest in startups, and what equity did he receive?
+31. How much did Kevin O'Leary typically invest in startups, and what equity did he receive?
