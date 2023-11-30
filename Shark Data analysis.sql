4af4d035628593b5069a7ca904c679f69d58453f
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-30 09:40:48 +0200

    Update Shark Data analysis.sql

--- Shark Data analysis.sql
+++ Shark Data analysis.sql
@@ -4,3 +4,7 @@
 
 -- 9. How was the gender distribution among the entrepreneurs who pitched their startups?
 -- 10. Which city and state were most entrepreneurs from?
+
+ How many of the pitches actually got a deal with the sharks?
+18. What was the total amount secured by startups in deals?
+19. What was the total equity given up by startups in deals?
