185e7332a58708b7bbb3991e94071f6d8dc47aee
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-30 09:39:09 +0200

    Update Shark Data analysis.sql

--- Shark Data analysis.sql
+++ Shark Data analysis.sql
@@ -1,3 +1,6 @@
 -- 1. What is the distribution of startups across different seasons of Shark Tank?
 
 -- 7. What industries were most commonly represented in the show?
+
+-- 9. How was the gender distribution among the entrepreneurs who pitched their startups?
+-- 10. Which city and state were most entrepreneurs from?
