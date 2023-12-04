cad1498f02bf5f85b3124b7175526c36c8fc8066
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-12-04 10:29:05 +0200

    Update Shark Data analysis.sql

--- Shark Data analysis.sql
+++ Shark Data analysis.sql
@@ -145,7 +145,7 @@ WHERE industry = "Food and Beverage" ;
 -- 16. Are they any trends in the industries or product categories that tend to receive higher valuation offers from the sharks?
 SELECT * FROM `Sharks Tank`;
 
-SELECT `Multiple Entrepreneurs`,  `Valuation Requested`, `Deal Valuation`
+SELECT `Multiple Entrepreneurs`,  `Original Ask Amount` - `Total Deal Amount`
 FROM `Sharks Tank`
 WHERE `Deal Valuation` IS NOT NULL;
 -- 17. Did startups with a larger number of entrepreneurs tend to secure more favorable deals?
