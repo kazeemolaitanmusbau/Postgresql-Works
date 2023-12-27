8598846ad2e0b86f16cd1b3f7994dd33d2bb82d9
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-12-27 17:58:29 +0200

    Update quick exercise.sql

--- quick exercise.sql
+++ quick exercise.sql
@@ -5,4 +5,10 @@ product_name varchar(29),
 sale_date DATE,
 quantity int,
 revenue int
-)
+);
+
+INSERT INTO sale
+VALUES(1, 'PRODUCT_A', '2023-01-01', 100,500),
+		(2, 'PRODUCT_B', '2023-01-01', 150,750),
+		(3, 'PRODUCT_A', '2023-01-02', 50,250),
+		(4, 'PRODUCT_C', '2023-01-03', 200,1000);
