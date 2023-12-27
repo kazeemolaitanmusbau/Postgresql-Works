533c260e91897f413e7995c19df0d32347ee2ceb
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-12-27 17:54:03 +0200

    Update quick exercise.sql

--- quick exercise.sql
+++ quick exercise.sql
@@ -1,1 +1,8 @@
---
+--using the table below, figure out what error in the statement below
+CREATE TABLE SALE(
+order_id int,
+product_name varchar(29),
+sale_date DATE,
+quantity int,
+revenue int
+)
